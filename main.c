95c1502ebb458392f204ec9c2e91a84e37b83850
nowakpawel <kontakt.nowakpawel@gmail.com>
2025-09-28 20:21:04 +0200

    feat: Add C Program with GitHub Actions workflow

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main() {
+	printf("Integer Types:\n");
+	printf("char: %d to %d\n", CHAR_MIN, CHAR_MAX); 
+	printf("short: %d to %d\n", SHRT_MIN, SHRT_MAX); 
+	printf("int: %d to %d\n", INT_MIN, INT_MAX); 
+	printf("long: %ld to %ld\n", LONG_MIN, LONG_MAX); 
+	printf("Char: %lld to %lld\n", LLONG_MIN, LLONG_MAX); 
+
+	printf("\nFloating-point types:\n");
+	printf("float: %e to %e\n", FLT_MIN, FLT_MAX); 
+	printf("double: %e to %e\n", DBL_MIN, DBL_MAX); 
+	printf("long double: %Le to %Le\n", LDBL_MIN, LDBL_MAX); 
+
+	return 0;
+}
